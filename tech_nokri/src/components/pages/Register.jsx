d9c440e326ff5eb2495e086810a81080bdbb80f2
namanjain0007 <namanjainayushjain7@gmail.com>
2024-11-18 19:01:05 +0530

    Update Register.jsx

--- tech_nokri/src/components/pages/Register.jsx
+++ tech_nokri/src/components/pages/Register.jsx
@@ -89,7 +89,7 @@ const Register = () => {
     // });
     try {
       const response = await axios.post(
-        "http://localhost:5012/register",
+        "https://tech-nokri1.onrender.com/register",
         formData,
         {
           headers: {
