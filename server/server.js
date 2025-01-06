f06376138646a81d40a3a11f99ccc0ed65db9ab4
namanjain0007 <namanjainayushjain7@gmail.com>
2025-01-06 18:36:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const jobApplicationRouter = require("./router/job_application-router");
 server.use(
   cors({
     origin: "https://tech-nokri1-frontend.onrender.com", // Frontend ka URL
-    // credentials: true, // Cookies allow karna
+    credentials: true, // Cookies allow karna
   })
 );
 
